f17909ed90d4a3e3bdfba8f87339854c92d098a5
Tra Mi Le <you@example.com>
2018-07-12 19:53:40 +0700

    S05

---
+++ Session05/session05 assignment/20.8.1.py
@@ -0,0 +1,12 @@
+#  Write a program that reads a string and returns a table of the letters of the alphabet in
+# alphabetical order which occur in the string together with the number of times each letter occurs. 
+
+
+my_string = input("Write a sentence: ")
+
+print("The sentence has:")
+
+letter_counts = {}
+for letter in my_string:
+    letter_counts[letter] = letter_counts.get(letter, 0) + 1
+print(letter_counts, end="\t")
